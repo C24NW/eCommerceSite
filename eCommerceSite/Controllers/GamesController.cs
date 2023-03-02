dc7d1e333444d184061539a04c48131ec1fac127
My Name <email@email.com>
2023-03-02 15:49:01 -0800

    Create game fully functional. Added styling.

--- eCommerceSite/Controllers/GamesController.cs
+++ eCommerceSite/Controllers/GamesController.cs
@@ -1,13 +1,41 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using eCommerceSite.Data;
+using eCommerceSite.Models;
+using Microsoft.AspNetCore.Mvc;
 
 namespace eCommerceSite.Controllers
 {
     public class GamesController : Controller
     {
+
+        private readonly VideoGameContext _context;
+
+        public GamesController(VideoGameContext context)
+        {
+            _context = context;
+        }
+
         [HttpGet]
         public IActionResult Create()
         {
             return View();
+        }
+
+        [HttpPost]
+        public IActionResult Create(Game g)
+        {
+            if (ModelState.IsValid)
+            {
+
+                //Adds game object g to database
+                _context.Games.Add(g); //prepares insert
+                _context.SaveChanges(); //runs insert
+
+                //Show success message on page
+                ViewData["Message"] = $"{g.Title} was added successfully!";
+
+                return View();
+            }
+            return View(g);
         }
     }
 }
