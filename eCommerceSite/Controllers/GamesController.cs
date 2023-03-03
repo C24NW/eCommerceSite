49b54ec1489e1e828e086a2c6bbaf7d4d1d62016
My Name <email@email.com>
2023-03-02 17:00:00 -0800

    Added HttpGet for Edit.

--- eCommerceSite/Controllers/GamesController.cs
+++ eCommerceSite/Controllers/GamesController.cs
@@ -52,5 +52,18 @@ namespace eCommerceSite.Controllers
             }
             return View(g);
         }
+
+
+        public async Task<IActionResult> Edit(int id)
+        {
+            Game? gameToEdit = await _context.Games.FindAsync(id);
+
+            if(gameToEdit == null)
+            {
+                return NotFound();
+            }
+
+            return View(gameToEdit);
+        }
     }
 }
